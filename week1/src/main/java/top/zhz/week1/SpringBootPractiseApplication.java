2ae62b29722f688215845d3de1268923a73239fc
zzh <1967430835z@gmail.com>
2025-09-03 11:37:41 +0800

    init: 初始化 spring-boot-practise 项目

---
+++ week1/src/main/java/top/zhz/week1/SpringBootPractiseApplication.java
@@ -0,0 +1,21 @@
+package top.zhz.week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPractiseApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello world";
+    }
+}
