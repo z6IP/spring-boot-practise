df24dab8c3eb097af69c913c8c6e898b40452dff
zzh <1967430835z@gmail.com>
2025-09-05 14:23:01 +0800

    feat:实现短信功能â

---
+++ spring-boot-quickstart/src/main/java/top/zhz/springboot/quickstart/config/CloopenConfig.java
@@ -0,0 +1,22 @@
+package top.zhz.springboot.quickstart.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @Author: zhz
+ * @Date: 2025/9/5
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "zhz.sms.ccp")
+public class CloopenConfig {
+    private String serverIp;
+    private String port;
+    private String accountSid;
+    private String accountToken;
+    private String appId;
+    private String templateId;
+}
