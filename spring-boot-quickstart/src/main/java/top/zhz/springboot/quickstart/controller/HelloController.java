2256d2f12d93604cb60d16ee592e0af5f484c85a
zzh <1967430835z@gmail.com>
2025-09-05 10:08:45 +0800

    feat:读取自定义配置,实现带开关的接口

--- spring-boot-quickstart/src/main/java/top/zhz/springboot/quickstart/controller/HelloController.java
+++ spring-boot-quickstart/src/main/java/top/zhz/springboot/quickstart/controller/HelloController.java
@@ -1,5 +1,6 @@
 package top.zhz.springboot.quickstart.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${my.mood.feature.helloSwitch}")
+    private boolean isHelloEnabled;
+
+    @Value("${my.mood.feature.closeMsg}")
+    private String closeMessage;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        if(isHelloEnabled){
+            return "接口开放中! 欢迎访问我的第一个 Spring Boot 项目~";
+        }else {
+            return closeMessage;
+        }
     }
 }
