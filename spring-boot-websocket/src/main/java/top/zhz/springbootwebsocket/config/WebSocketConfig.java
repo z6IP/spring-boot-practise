ba7bb417855426b317d765e03a2d83d4a20779d5
zzh <1967430835z@gmail.com>
2025-10-11 16:13:32 +0800

    feat:WebSocket学习

---
+++ spring-boot-websocket/src/main/java/top/zhz/springbootwebsocket/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package top.zhz.springbootwebsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import top.zhz.springbootwebsocket.handler.SimpleTimeWebSocketHandler;
+import top.zhz.springbootwebsocket.handler.TestWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 测试的 WebSocket 端点
+        registry.addHandler(new TestWebSocketHandler(), "/ws/test")
+                .setAllowedOrigins("*");
+
+        registry.addHandler(new SimpleTimeWebSocketHandler(), "/ws/time").setAllowedOrigins("*");
+    }
+}
